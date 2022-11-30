4eff1882c5c73f889dc85582ce22bb81c338972c
Alden D Muaja <lcomputer04@gmail.com>
2022-12-01 00:47:39 +0800

    commit final project

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function index(){
+    return(
+       <footer>
+  <div className="container">
+    <div className="row">
+      <div className="col-sm-12">
+        <div className="copyright-box">
+          <p className="copyright"><strong>Alden D Muaja</strong></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+    )
+}
+
+export default index
